fc4f3c8b68f1b8ef5422fd41216c2fa3505918d9
Wojtek <wojga476@gmail.com>
2018-06-20 14:01:51 +0200

    poprawa-stworzenie tablicy losowych liczb z odbiciem lustrzanym

--- src/TabLosLiczb.java
+++ src/TabLosLiczb.java
@@ -4,15 +4,20 @@ public class TabLosLiczb {
     public static void main(String[] args) {
 
         int[] tab = new int[10];
-        for(int i=0; i<tab.length; i++) {
-            Random rand = new Random();
-            tab[i] = rand.nextInt(10);
-            System.out.println(tab[i]);
+        Random rand = new Random(10);
+        for(int liczba=0;liczba<10;liczba++) {
+            tab[liczba] = rand.nextInt(100);
         }
-        for(int x=tab.length-1;x>0;x--){
-            System.out.println(tab[x]);
+        int a =9;
+        for (int i = 0; i < 21; i++) {
+
+            if(i<10) {
+                System.out.print(tab[i] + " ");
+            }
+            else if (i>10) {
+                System.out.print(tab[a] + " ");
+                a-=1;
+            }
         }
     }
-
-
 }
